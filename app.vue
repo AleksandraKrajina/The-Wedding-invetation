fbf0cc27f2dfbacdd49c57379bbec66c5d300bc6
luka.m.savovic <luka.m.savovic@gmail.com>
2025-11-20 10:00:12 +0100

    refactor

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
+  <Analytics/>
   <div>
     <NuxtPage></NuxtPage>
   </div>
 </template>
 
 <script setup>
+import { Analytics } from '@vercel/analytics/nuxt'
 import 'primeicons/primeicons.css'
 </script>
